5856b6e042e4b05815f3054a0f7fe96186bdc9b1
Paul Zanna <paul@northboundnetworks.com>
2017-11-13 22:25:53 +1100

    save

--- package/luci-app-nnofagent/files/model/cbi/packet-armour.lua
+++ package/luci-app-nnofagent/files/model/cbi/packet-armour.lua
@@ -1,5 +1,5 @@
-m = Map("nnofagent", "Control", "Here you can set the Network Control configuration.")
-s = m:section(TypedSection, "nnofagent", "Packet Armour Settings")
+m = Map("packet-armour", "Control", "Here you can set the Network Control configuration.")
+s = m:section(TypedSection, "packet-armour", "Packet Armour Settings")
 s.anonymous = true
 
 e = s:option(Flag, "hosts", "Enable Host Verification")
