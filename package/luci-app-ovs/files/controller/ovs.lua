c950a120104dc6f286d51b1bf526e18b5943a7b2
Paul Zanna <paul@northboundnetworks.com>
2018-06-21 09:44:12 +1000

    Updated luci-app-ovs

--- package/luci-app-ovs/files/controller/ovs.lua
+++ package/luci-app-ovs/files/controller/ovs.lua
@@ -3,26 +3,33 @@ module("luci.controller.ovs", package.seeall)
 function index()
 
 	entry({"admin", "OpenFlow"}, firstchild(), "OpenFlow", 60).dependent=false
-	entry({"admin", "OpenFlow", "flowtable"}, call("action_flowtable"), _("Flow Table"), 3)
+	entry({"admin", "OpenFlow", "flows"}, call("action_flows"), _("Flows"), 3)
 	entry({"admin", "OpenFlow", "groups"}, call("action_groups"), _("Groups"), 4)
-	entry({"admin", "OpenFlow", "meters"}, call("action_meters"), _("Meters"), 5)
+	entry({"admin", "OpenFlow", "ports"}, call("action_ports"), _("Ports"), 5)
+	entry({"admin", "OpenFlow", "tables"}, call("action_tables"), _("Tables"), 6)
 
 end
 
--- Get flow table output
-function action_flowtable()
-	local flowtable = luci.sys.exec("ovs-ofctl dump-flows ovslan")
-	luci.template.render("flowtable", {flowtable=flowtable})
+-- Get flow output
+function action_flows()
+	local flows = luci.sys.exec("ovs-ofctl -O OpenFlow13 dump-flows ovslan")
+	luci.template.render("flows", {flows=flows})
 end
 
 -- Get groups output
 function action_groups()
-	local groups = luci.sys.exec("ovs-ofctl dump-groups ovslan")
+	local groups = luci.sys.exec("ovs-ofctl -O OpenFlow13 dump-groups ovslan")
 	luci.template.render("groups", {groups=groups})
 end
 
--- Get meters output
-function action_meters()
-	local meters = luci.sys.exec("ovs-ofctl dump-meters ovslan")
-	luci.template.render("meters", {meters=meters})
+-- Get ports output
+function action_ports()
+	local ports = luci.sys.exec("ovs-ofctl -O OpenFlow13 dump-ports ovslan")
+	luci.template.render("ports", {ports=ports})
+end
+
+-- Get table output
+function action_tables()
+	local tables = luci.sys.exec("ovs-ofctl -O OpenFlow13 dump-tables ovslan")
+	luci.template.render("tables", {tables=tables})
 end
